54ab679a02dcfd21dccc76b3f8f6038e00e2e327
saialeti <sai49577@gmail.com>
2025-07-13 11:34:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,6 @@ def place_trade():
         print("❌ Error placing trade:", e)
         return jsonify({"error": str(e)}), 500
 
-
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000)
+
